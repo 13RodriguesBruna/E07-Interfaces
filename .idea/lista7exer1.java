8b391f82a31fad5d51ff59015e1864fb45a5bfa0
13RodriguesBruna <107412241+13RodriguesBruna@users.noreply.github.com>
2022-10-06 18:25:10 -0300

    Add files via upload

---
+++ .idea/lista7exer1.java
@@ -0,0 +1,27 @@
+ public class Main {
+        public static void main(String[]args){
+        cliente pfisi = new ClientePF();
+        cliente pjuri = new ClientePJ();
+        
+        if(pfisi.autenticar(()){
+        System.out.println("A conta está autenticada");
+        
+        }else {
+        System.out.println("Conta não está autenticada);
+        a
+        }
+
+
+        }
+
+//METODO AUTENTICAR, vou usar chave dono e cpf
+            //ClientePessoaFisica: A autenticação deverá comparar se a chave é igual ao CPF.
+             public boolean autenticar1(String chave){
+
+                 if(chave.equals(this.numero)){
+                     return true;a
+            }
+                 else
+                     return false;
+             }
+        }
